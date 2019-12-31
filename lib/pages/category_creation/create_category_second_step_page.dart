b246466173dd898f02b7e9834818b3282f38289e
Natan Portilho <natanportilho@outlook.com>
2019-12-31 18:44:14 -0300

    Creating form for category creation

--- lib/pages/category_creation/create_category_second_step_page.dart
+++ lib/pages/category_creation/create_category_second_step_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CreateCategorySecondStepPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   final String imgUrl;
+  String name;
   CreateCategorySecondStepPage(this.imgUrl);
 
   @override
@@ -14,12 +17,53 @@ class CreateCategorySecondStepPage extends StatelessWidget {
       ),
       body: Material(
         child: Center(
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(this.imgUrl),
-            radius: 80,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(this.imgUrl),
+                  radius: 80,
+                ),
+                _createTodoForm(context)
+              ],
+            ),
           ),
         ),
       ),
     ));
+  }
+
+  Form _createTodoForm(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _createNameField(),
+          RaisedButton(
+            onPressed: () {
+              if (_formKey.currentState.validate()) {}
+            },
+            child: Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  TextFormField _createNameField() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        hintText: 'Name',
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter a name for the category';
+        }
+        name = value;
+        return null;
+      },
+    );
   }
 }
