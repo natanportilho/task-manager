c75c4abbc47a66a10b9cc67c87fddebb8f915e31
Natan Portilho <natanportilho@outlook.com>
2020-01-04 11:33:59 -0300

    Adding database method for categoryModel

--- lib/model/category_mode.dart
+++ lib/model/category_mode.dart
@@ -1,6 +1,21 @@
 class CategoryModel {
+  String id;
   String name;
   String imageUrl;
 
   CategoryModel(this.name, this.imageUrl);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  CategoryModel.fromList(List<dynamic> categoryInfo) {
+    this.id = categoryInfo[0].toString();
+    this.name = categoryInfo[2];
+    this.imageUrl = categoryInfo[2];
+  }
 }
