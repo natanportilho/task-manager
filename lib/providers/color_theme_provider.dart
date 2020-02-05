d86266991a2305840dfd50d99f5a671d4aba7b6d
Natan Portilho <natanportilho@outlook.com>
2020-02-05 20:29:53 -0300

    Adding todo

--- lib/providers/color_theme_provider.dart
+++ lib/providers/color_theme_provider.dart
@@ -5,6 +5,7 @@ import 'package:task_manager/persistence/color_theme.dart';
 class ColorThemeProvider extends ChangeNotifier {
   ColorTheme color;
 
+// TODO: Should save theme as preferences for app
   Future setColor(ColorTheme color) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString('color', color.name);
