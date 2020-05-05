6e22ed21b614acd4dbe0bd457be40f174cf713ac
Natan Portilho <natanportilho@outlook.com>
2020-05-04 22:31:46 -0300

    Creating category dropdown now using mobx

--- lib/pages/create_todo_page.dart
+++ lib/pages/create_todo_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 // import 'package:task_manager/persistence/todo_table.dart';
 import 'package:task_manager/providers/category_dropdown_provider.dart';
 import 'package:task_manager/providers/category_provider.dart';
 import 'package:task_manager/providers/color_theme_provider.dart';
+import 'package:task_manager/stores/store_category.dart';
 import 'package:task_manager/stores/task_store.dart';
 import 'package:task_manager/models/task_model.dart';
 import 'package:task_manager/models/category_model.dart';
@@ -20,6 +22,7 @@ class CreateTodoPage extends StatefulWidget {
 }
 
 class _CreateTodoPageState extends State<CreateTodoPage> {
+  CategoryStore categoryStore;
   final _formKey = GlobalKey<FormState>();
   String category;
   String name;
@@ -69,7 +72,7 @@ class _CreateTodoPageState extends State<CreateTodoPage> {
                 'Category:    ',
                 style: TextStyle(fontSize: 16.0),
               ),
-              CategoryDropdown(),
+              Observer(builder: (_) => _createDropDown()),
               IconButton(
                 icon: Icon(Icons.add),
                 color: Colors.purpleAccent[700],
@@ -140,5 +143,40 @@ class _CreateTodoPageState extends State<CreateTodoPage> {
         MaterialPageRoute(
           builder: (context) => CreateCategoryPage(),
         ));
+  }
+
+  DropdownButton _createDropDown() {
+    return DropdownButton<String>(
+      items: _createCategoriesList().toList(),
+      onChanged: (newValue) {
+        category = newValue;
+      },
+      underline: Container(
+        height: 2,
+        color: Colors.green,
+      ),
+      icon: Icon(Icons.arrow_downward),
+      value: category != null ? category : "Personal",
+    );
+  }
+
+  List<DropdownMenuItem<String>> _createCategoriesList() {
+    List<DropdownMenuItem<String>> categoriesNames =
+        List<DropdownMenuItem<String>>();
+    if (categoryStore.categories != null) {
+      categoryStore.categories.forEach((c) => {
+            categoriesNames.add(DropdownMenuItem<String>(
+              value: c.name,
+              child: Text(
+                c.name,
+                style: new TextStyle(
+                  fontSize: 12.0,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+            )),
+          });
+    }
+    return categoriesNames;
   }
 }
