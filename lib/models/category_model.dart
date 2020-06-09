190711ae8eb2b554cd8eef1a02aaac8436e4b198
Natan Portilho <natanportilho@outlook.com>
2020-06-09 20:39:13 -0300

    Adding category image url to Category and making category attributes observable

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,18 +1,29 @@
-class Category {
+import 'package:mobx/mobx.dart';
+part 'category_model.g.dart';
+
+class Category = _Category with _$Category;
+
+abstract class _Category with Store {
+  @observable
   int id;
+  @observable
   String name;
+  @observable
+  String imageUrl;
 
-  Category({this.id, this.name});
+  _Category({this.id, this.name});
 
-  Category.fromJson(Map<String, dynamic> json) {
+  _Category.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     name = json['name'];
+    imageUrl = json['imageUrl'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
     data['name'] = this.name;
+    data['imageUrl'] = this.imageUrl;
     return data;
   }
 }
