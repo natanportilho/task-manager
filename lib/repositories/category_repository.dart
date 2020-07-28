4a6d47ba78856ec33e79bb702129e06355d95066
Natan Portilho <natanportilho@outlook.com>
2020-07-28 19:04:19 -0300

    Adding methods to get categories from firebase

--- lib/repositories/category_repository.dart
+++ lib/repositories/category_repository.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:task_manager/repositories/category_repository_interface.dart';
 
 class CategoryRepository implements ICategoryRepository {
+  Firestore firestore;
+  CollectionReference _userReference;
   @override
   Future<DocumentReference> addCategory(Category category) {
     // TODO: implement addCategory
@@ -11,8 +13,17 @@ class CategoryRepository implements ICategoryRepository {
 
   @override
   Stream<List<Category>> getCategories() {
-    // TODO: implement getCategories
-    throw UnimplementedError();
+    this.firestore = Firestore.instance;
+    return this.firestore.collection('category').snapshots().map((query) {
+      return query.documents.map((doc) {
+        return fromDocument(doc);
+      }).toList();
+    });
   }
 
+// Why cant I have this in the model?
+  Category fromDocument(DocumentSnapshot doc) {
+    return Category(
+        name: doc['name'], imageUrl: doc['imageUrl'], id: doc.reference);
+  }
 }
