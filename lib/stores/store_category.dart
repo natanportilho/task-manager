f53dd2029aa792b7cd7a2e493ee808acfda37472
Natan Portilho <natanportilho@outlook.com>
2020-07-31 08:19:39 -0300

    Implementing method to add category to firebase

--- lib/stores/store_category.dart
+++ lib/stores/store_category.dart
@@ -1,39 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mobx/mobx.dart';
 import 'package:task_manager/models/category_model.dart';
+import 'package:task_manager/repositories/category_repository.dart';
+import 'package:task_manager/repositories/category_repository_interface.dart';
 part 'store_category.g.dart';
 
 class CategoryStore = _CategoryStore with _$CategoryStore;
 
 abstract class _CategoryStore with Store {
-
   // Shoudl get categories from Firebase
-  _CategoryStore() {
-    // categories.add(Category(
-    //     id: 1,
-    //     name: "Personal",
-    //     imageUrl:
-    //         'https://images.unsplash.com/photo-1462926703708-44ab9e271d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80'));
-    // categories.add(Category(
-    //     id: 2,
-    //     name: "Study",
-    //     imageUrl:
-    //         'https://images.unsplash.com/photo-1494498902093-87f291949d17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'));
-    // categories.add(Category(
-    //     id: 3,
-    //     name: "Work",
-    //     imageUrl:
-    //         'https://images.unsplash.com/photo-1537202108838-e7072bad1927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=685&q=80'));
-  }
+  ICategoryRepository categoryRepository = CategoryRepository();
 
   @observable
-  var categories = ObservableList<Category>();
+  ObservableStream<List<Category>> categories;
+
+  _CategoryStore() {
+    getCategories();
+  }
 
   @action
-  void add(Category category) {
-    categories.add(category);
+  ObservableList<Category> getCategories() {
+    categories = categoryRepository.getCategories().asObservable();
   }
 
-  Category getCategoryByName(String name) {
-    return categories.firstWhere((c) => c.name == name);
+  @action
+  Future<void> add(Category category) async {
+    DocumentReference ref = await categoryRepository.addCategory(category);
+    category.id = ref;
+
+
+    // categories.add(category);
+    // todo: how do I get the id generated from firebase and have it also in my taskStore?
+    //todoRepository.addTodo(task);
   }
+
+  // Category getCategoryByName(String name) {
+  //   return categories.firstWhere((c) => c.name == name);
+  // }
 }
