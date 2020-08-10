a1a0b9c8aa874e2765ff2d5c3743c104b21de7b5
Natan Portilho <natanportilho@outlook.com>
2020-08-10 18:48:10 -0300

    Removing comments

--- lib/stores/task_store.dart
+++ lib/stores/task_store.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mobx/mobx.dart';
-import 'package:task_manager/models/category_model.dart';
 import 'package:task_manager/models/task_model.dart';
 import 'package:task_manager/repositories/todo_repository.dart';
 import 'package:task_manager/repositories/todo_repository_interface.dart';
@@ -8,7 +7,6 @@ part 'task_store.g.dart';
 
 class TaskStore = _TaskStore with _$TaskStore;
 
-//TODO: This class must create TaskModels in firebase (this works) and update those as well as get them (How do we update the observable list?)
 abstract class _TaskStore with Store {
   ITodoRepository todoRepository = TodoRepository();
 
@@ -35,39 +33,9 @@ abstract class _TaskStore with Store {
     task.id = ref;
   }
 
-  // @action
-  // Task updateCategory(String taskId, Category category) {
-  //   _updateTask(getTaksById(taskId)..category = category);
-  // }
-
-  // @action
-  // void toggleDoneFlag(String taskId) {
-  //   Task task = getTaksById(taskId);
-  //   task.done = !task.done;
-  //   _updateTask(task);
-  // }
-
   @action
   updateDescription(Task task, String description) {
     task.description = description;
     todoRepository.updateDescription(task);
-    //_updateTask(getTaksById(taskId)..description = description);
   }
-
-  // @action
-  // void remove(Task task) {
-  //   tasks.remove(task);
-  // }
-
-  // Task getTaksById(String taskId) {
-  //   return tasks.firstWhere((t) => t.id == taskId);
-  // }
-
-  // _updateTask(Task task) {
-  //   for (var i = 0; i < tasks.length; i++) {
-  //     if (tasks[i].id == task.id) {
-  //       tasks[i] = task;
-  //     }
-  //   }
-  // }
 }
