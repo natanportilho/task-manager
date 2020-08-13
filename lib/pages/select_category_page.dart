2e742877aae8f48cabbb4c594b55e001ed1877a1
Natan Portilho <natanportilho@outlook.com>
2020-08-13 13:36:47 -0300

    Adding null check to avoid null pointer exception when getting categories from firebase

--- lib/pages/select_category_page.dart
+++ lib/pages/select_category_page.dart
@@ -1,40 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:task_manager/models/category_model.dart';
+import 'package:task_manager/stores/store_category.dart';
 
 //TODO: This page should be used to update categories
 class SelectCategoryPage extends StatelessWidget {
+  CategoryStore categoryStore;
   final String todoId;
   String name;
   SelectCategoryPage(this.todoId);
 
   @override
   Widget build(BuildContext context) {
+    categoryStore = Provider.of<CategoryStore>(context);
 
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(
-        title: Text('Select Category'),
-      ),
-      body: Material(
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: GridView.count(
-              crossAxisCount: 4,
-              children:
-                  List.generate(10, (index) {
-                return Center(
-                  child: GestureDetector(
-                    onTap: () => {
-                    },
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          ''),
-                      maxRadius: 40,
-                    ),
-                  ),
-                );
-              }),
-            )),
-      ),
-    ));
+            appBar: AppBar(
+              title: Text('Select Category'),
+            ),
+            body: Material(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Observer(builder: (_) => _buildGridView()),
+              ),
+            )));
+  }
+
+  GridView _buildGridView() {
+    List<Category> allcategories = <Category>[];
+
+    if (categoryStore.categories != null &&
+        categoryStore.categories.data != null) {
+      categoryStore.categories.data.forEach((c) => {
+            allcategories.add(c),
+          });
+    }
+
+    return GridView.count(
+      crossAxisCount: 4,
+      children: List.generate(allcategories.length, (index) {
+        return Center(
+          child: GestureDetector(
+            onTap: () => {},
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(allcategories[index].imageUrl),
+              maxRadius: 40,
+            ),
+          ),
+        );
+      }),
+    );
   }
 }
