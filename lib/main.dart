58f9bb043469c94b6b967b5a1f36b0c564a117ea
Natan Portilho <natanportilho@outlook.com>
2020-09-01 11:31:31 -0300

    Adding necessary configuration for flutter notifications package in Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:task_manager/stores/store_category.dart';
 import 'package:task_manager/stores/task_store.dart';
@@ -6,10 +7,41 @@ import 'package:dynamic_theme/dynamic_theme.dart';
 
 import 'pages/home_page.dart';
 
-void main() => runApp(MultiProvider(providers: [
-      Provider<TaskStore>(create: (_) => TaskStore()),
-      Provider<CategoryStore>(create: (_) => CategoryStore())
-    ], child: MyApp()));
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() => {
+      initializeLocalNotifications(),
+      runApp(
+        MultiProvider(providers: [
+          Provider<TaskStore>(create: (_) => TaskStore()),
+          Provider<CategoryStore>(create: (_) => CategoryStore())
+        ], child: MyApp()),
+      ),
+    };
+
+initializeLocalNotifications() async {
+  WidgetsFlutterBinding.ensureInitialized();
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('ic_launcher');
+
+  var initializationSettingsIOS = IOSInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification:
+          (int id, String title, String body, String payload) async {});
+
+  var initializationSettings = InitializationSettings(
+      initializationSettingsAndroid, initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+  });
+}
 
 class MyApp extends StatelessWidget {
   @override
