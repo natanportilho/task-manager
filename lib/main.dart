a49101d8aa3a885bf5a200abb1df1b4e0bc28020
Natan Portilho <natanportilho@outlook.com>
2020-09-02 14:00:40 -0300

    Moving notifications code to new class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
+import 'package:task_manager/services/notifications_service.dart';
 import 'package:task_manager/stores/store_category.dart';
 import 'package:task_manager/stores/task_store.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
@@ -11,7 +12,7 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
 void main() => {
-      initializeLocalNotifications(),
+      _initializeLocalNotifications(),
       runApp(
         MultiProvider(providers: [
           Provider<TaskStore>(create: (_) => TaskStore()),
@@ -20,27 +21,9 @@ void main() => {
       ),
     };
 
-initializeLocalNotifications() async {
-  WidgetsFlutterBinding.ensureInitialized();
-// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-  var initializationSettingsAndroid =
-      AndroidInitializationSettings('ic_launcher');
-
-  var initializationSettingsIOS = IOSInitializationSettings(
-      requestAlertPermission: true,
-      requestBadgePermission: true,
-      requestSoundPermission: true,
-      onDidReceiveLocalNotification:
-          (int id, String title, String body, String payload) async {});
-
-  var initializationSettings = InitializationSettings(
-      initializationSettingsAndroid, initializationSettingsIOS);
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-      onSelectNotification: (String payload) async {
-    if (payload != null) {
-      debugPrint('notification payload: ' + payload);
-    }
-  });
+_initializeLocalNotifications() async {
+  NotificationsService notificationsService = NotificationsService();
+  notificationsService.initializeLocalNotifications();
 }
 
 class MyApp extends StatelessWidget {
