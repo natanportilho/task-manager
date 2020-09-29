5c2355c786984a308579b1c915aec024e81e04cd
Natan Portilho <natanportilho@outlook.com>
2020-09-29 19:38:40 -0300

    Removing unnecessary variable

--- lib/repositories/todo_repository.dart
+++ lib/repositories/todo_repository.dart
@@ -9,7 +9,6 @@ class TodoRepository implements ITodoRepository {
 
   @override
   Stream<List<Task>> getTodos() {
-    this.firestore = FirebaseFirestore.instance;
     return this.firestore.collection('task').snapshots().map((query) {
       return query.docs.map((doc) {
         return fromDocument(doc);
@@ -18,13 +17,10 @@ class TodoRepository implements ITodoRepository {
   }
 
   void addTodo(Task task) {
-    this.firestore = FirebaseFirestore.instance;
     this.firestore.collection('task').add(task.toJson());
   }
 
   void toggleTodo(Task task) {
-    this.firestore = FirebaseFirestore.instance;
-
     task.done = !task.done;
     if (task != null) {
       this
@@ -36,8 +32,6 @@ class TodoRepository implements ITodoRepository {
   }
 
   void toggleImportant(Task task) {
-    this.firestore = FirebaseFirestore.instance;
-
     task.important = !task.important;
     if (task != null) {
       this
@@ -49,8 +43,6 @@ class TodoRepository implements ITodoRepository {
   }
 
   void updateDescription(Task task) {
-    this.firestore = FirebaseFirestore.instance;
-
     if (task != null) {
       this
           .firestore
@@ -61,8 +53,6 @@ class TodoRepository implements ITodoRepository {
   }
 
   void updateCategory(Task task) {
-    this.firestore = FirebaseFirestore.instance;
-
     if (task != null) {
       this
           .firestore
@@ -73,8 +63,6 @@ class TodoRepository implements ITodoRepository {
   }
 
   void remove(Task task) {
-    this.firestore = FirebaseFirestore.instance;
-
     if (task != null) {
       this.firestore.collection("task").doc(task.id.id).delete();
     }
