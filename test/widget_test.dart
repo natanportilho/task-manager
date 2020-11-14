56803119aba940aad2c7aa8f47b2877c3b9f4d3f
Natan Portilho <natanportilho@outlook.com>
2020-11-14 18:19:01 -0300

    Creating new test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,33 +6,78 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
 import 'package:task_manager/models/task_model.dart';
+import 'package:task_manager/pages/home_page.dart';
 import 'package:task_manager/repositories/category_repository_interface.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:task_manager/repositories/todo_repository.dart';
 import 'package:task_manager/repositories/todo_repository_interface.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:task_manager/services/firebase_service.dart';
+import 'package:task_manager/stores/store_category.dart';
 import 'package:task_manager/stores/task_store.dart';
-import 'package:mobx/mobx.dart' as mobx;
 
 //class TodoRepositoryMock extends Mock implements ITodoRepository {}
 
-class CategoryRepositoyMock extends Mock implements ICategoryRepository {}
+class CategoryRepositoyMock extends Mock implements ICategoryRepository {
+  CategoryRepositoyMock(FirestoreMock firestore);
+}
+
+class TodoRepositoryMock extends Mock implements ITodoRepository {
+  TodoRepositoryMock(MockFirestoreInstance firestore);
+}
 
 class DocumentReferenceMock extends Mock implements DocumentReference {}
 
-class FirestoreMock extends Mock implements Firestore {}
+class FirestoreMock extends Mock implements FirebaseFirestore {}
 
 class TaskMock extends Mock implements Task {}
 
 class TaskStoreMock extends Mock implements TaskStore {}
 
+class CategoryStoreMock extends Mock implements CategoryStore {}
+
 class CollectionReferenceMock extends Mock implements CollectionReference {}
+
+class FirebaseServiceMock extends Mock {
+  static final FirebaseServiceMock _singleton = FirebaseServiceMock._internal();
+  MockFirestoreInstance instance = MockFirestoreInstance();
+
+  factory FirebaseServiceMock() {
+    return _singleton;
+  }
+
+  FirebaseServiceMock._internal();
+}
 
 void main() {
   group("Test Pages", () {
-    testWidgets("Test Todo Page", (WidgetTester tester) async {});
+    test('get tasks', () async {
+      FirebaseServiceMock firebaseServiceMock = FirebaseServiceMock();
+
+      await firebaseServiceMock.instance.collection('task').add(
+          {'description': 'Hello world!', 'done': false, 'important': true});
+
+      final DocumentReferenceMock mockDocumentRef = DocumentReferenceMock();
+      Task task = Task();
+      task.id = mockDocumentRef;
+      task.description = 'lololo';
+      task.done = true;
+      task.important = false;
+
+      final TaskStore taskStore =
+          TaskStore.testConstructor(firebaseServiceMock.instance);
+
+      taskStore.add(task);
+
+      var todos = await taskStore.todos.data;
+
+      // this is null but task store has the values. Why is it null?
+      print(todos);
+    });
   });
 }
